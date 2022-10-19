6fd98ac31106f32c401a1b877bb4f0eb5c8871a9
Mahesh <maheshreddymallidi01@gmail.com>
2022-10-19 13:41:25 +0530

    SpringBootApp

--- SpringBootPractise/src/main/java/com/springboot/practise/CourseApiApp.java
+++ SpringBootPractise/src/main/java/com/springboot/practise/CourseApiApp.java
@@ -1,10 +1,13 @@
 package com.springboot.practise;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CourseApiApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(CourseApiApp.class, args);
 	}
 
 }
