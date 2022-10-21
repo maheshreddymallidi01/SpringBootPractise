9fc9db0a3fbbe1b06704eb9564f1315ea4dd7fda
Mahesh <maheshreddymallidi01@gmail.com>
2022-10-21 13:57:32 +0530

    CRUD

--- SpringBootPractise/src/main/java/com/springboot/practise/controller/TopicController.java
+++ SpringBootPractise/src/main/java/com/springboot/practise/controller/TopicController.java
@@ -3,19 +3,43 @@ package com.springboot.practise.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.practise.service.TopicService;
 
 @RestController
 public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
 
 	@RequestMapping("/topics")
-	public List<Topic> getTopic() {
-		return Arrays.asList(
-				new Topic("Spring","SpringBoot Framework","Spring Framework Description"),
-				new Topic("Java","J2EE","Java Description"),
-				new Topic("React","React Library","React Library Description")
-				);
+	public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
 	}
 
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(topic,id);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
 }
